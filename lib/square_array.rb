33e17274eebb07a73e50f57a016c4995e6e32a75
Stefan Lewyckyj <s80lewyckyj@outlook.com>
2020-08-18 01:10:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
    array.length.times do |index|
-
-    new_array.push((array[index] + 1)**2)
+   new_array.push(array[index] ** 2)
+   
   end
   new_array
 end
